7f3364798168b44b530e2a89ac5cd27bc465682a
MOAZZAM ALI SANI <136630501+MA-Sani@users.noreply.github.com>
2023-11-16 23:17:10 +0500

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,31 @@
+public interface Shape {
+    double calculateArea();
+}
+
+public class Circle implements Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Rectangle implements Shape {
+    double length;
+    double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
